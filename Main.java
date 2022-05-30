7baca71e2be04305dd241f0eee4879405c3fe1d7
art_time Шайхуллин <a6968271@gmail.com>
2022-05-30 12:24:45 +0000

    Hw1

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String name = "Valentin";
+    String surname = "Zaicev";
+    int income = 20000;
+    int spending = 10000;
+    int surplus = income - spending;
+    System.out.println(name + " " + surname + " " + surplus + "руб");
   }
 }
